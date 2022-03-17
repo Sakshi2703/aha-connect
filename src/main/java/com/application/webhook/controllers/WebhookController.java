3354a18539b35ff88f3b396837303e3f9e20b07f
Nigam <Sakshi.Nigam@mastercard.com>
2022-03-17 21:33:07 +0530

    added files

---
+++ src/main/java/com/application/webhook/controllers/WebhookController.java
@@ -0,0 +1,31 @@
+package com.application.webhook.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.application.webhook.services.WebhookService;
+
+@RestController
+public class WebhookController {
+	
+	@Autowired
+	private WebhookService webhookService;
+	
+	@GetMapping("/contents")
+	public Object getDataFromAha() {
+		Object data = webhookService.fetchContents();
+		return new ResponseEntity<Object>(data,HttpStatus.OK);
+	}
+	
+	@PostMapping("/webhook")
+	public Object fetchDataFromAha(@RequestBody String requestPayload) {
+		Object data = webhookService.saveContents(requestPayload);
+		return new ResponseEntity<Object>(data,HttpStatus.CREATED);
+	}
+
+}
---
+++ src/main/java/com/application/webhook/services/WebhookService.java
@@ -0,0 +1,29 @@
+package com.application.webhook.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.Gson;
+
+@Service
+public class WebhookService {
+	
+	List<Object> contents = new ArrayList();
+	
+	public Object saveContents(String requestData) {
+		Gson gson = new Gson();
+		Object element = gson.fromJson(requestData, Object.class);
+		contents.add(element);
+		return element;
+	}
+	
+	public Object fetchContents() {
+		return contents;
+	}
+}
